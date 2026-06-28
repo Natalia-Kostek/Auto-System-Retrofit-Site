85e6f1f4bbe9f46168631774ccf830eb7a742570
Natalia-Kostek <kosteknatalia83@gmail.com>
2026-06-28 18:58:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,11 @@ window.addEventListener("scroll", () => {
     header.classList.remove("scrolled");
   }
 });
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  if(loader){
+    loader.style.opacity = "0";
+    loader.style.pointerEvents = "none";
+    setTimeout(() => loader.remove(), 500);
+  }
+});
