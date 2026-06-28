3bd32bdf074d6a697f0e2efc63a4f2f4af9a82be
Natalia-Kostek <kosteknatalia83@gmail.com>
2026-06-28 13:00:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// MENU MOBILE
-function toggleMenu(){
-  document.getElementById("nav").classList.toggle("active");
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
-
-// ANIMACJE SCROLL
-const elements = document.querySelectorAll(".card, .photo");
-
-const observer = new IntersectionObserver(entries=>{
-  entries.forEach(e=>{
-    if(e.isIntersecting){
-      e.target.style.opacity=1;
-      e.target.style.transform="translateY(0)";
-    }
-  });
-});
-
-elements.forEach(el=>{
-  el.style.opacity=0;
-  el.style.transform="translateY(30px)";
-  el.style.transition="0.6s";
-  observer.observe(el);
-});
