d5d7a829db4d55ff6e2badbf285996bece201eed
Natalia-Kostek <kosteknatalia83@gmail.com>
2026-07-04 20:27:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const thumbs = document.getElementById("lightbox-thumbs");
 const categories = {
     haki: 35,
     instalacje: 10,
+    wiazki: 14,
     radio_kamery: 14,
     czujniki: 16,
     podcinanie_zderzaka: 4,
